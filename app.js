e2b8871ab23467f544b9ba420753738fbeb609bd
Ulan Bekboev <ubekboouulu@gmail.com>
2023-07-15 01:15:36 +0600

    feat: 'bugs are fixed'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const NotFoundError = require('./validationErrors/NotFoundError');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -30,6 +31,8 @@ app.use('/cards', require('./routes/cards'));
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => { // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
